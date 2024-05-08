86ad343b84f23cfa3492561969b0429d4575c09f
NozoDev <kevinroj326@gmail.com>
2024-05-08 06:51:14 -0500

    creando la db mokeada, y agregando pequeños comentrarios para saber que es cada cosa. que no vaya entendiendo.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import './index.css'
-
+import {menuItems} from './data/db'
 function App() {
-
+console.log(menuItems)
 
   return (
     <>
-      <h1 className='bg-red-500'>Hola mundo</h1>
+      <header className='bg-teal-700 py-4'>
+        <h1 className='text-center font-bold text-4xl font-mono'>Calculadora de propinas</h1>
+      </header>
     </>
   )
 }
