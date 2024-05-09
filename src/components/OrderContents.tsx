deb4e795fdf3943398b58965f9f45d41cd9e65c8
NozoDev <kevinroj326@gmail.com>
2024-05-09 06:39:32 -0500

    estilos a consumo

--- src/components/OrderContents.tsx
+++ src/components/OrderContents.tsx
@@ -1,3 +1,4 @@
+import formatCurrency from "../helpers";
 import { orderItem } from "../types";
 
 type OrderContentsProps = {
@@ -9,13 +10,27 @@ const OrderContents = ({ order }: OrderContentsProps) => {
     <div>
       <h2 className="font-black text-4xl">Consumo</h2>
 
-      <div className="space-y-3 mt-5">
+      <div className="space-y-3 mt-10">
         {order.length === 0 ? (
           <p className="text-center"> La orden esta vacia.</p>
         ) : (
           order.map((item) => (
-            <div key={item.id}>
-              <p>{item.name}</p>
+            <div
+              className="flex justify-between items-center border-t border-gray-500 py-5 last-of-type:border-b"
+              key={item.id}
+            >
+              <div>
+                <p className="text-lg">
+                  {item.name} - {formatCurrency(item.price)}
+                </p>
+                <p className="font-black">
+                  Cantidad: {item.quantity} -{" "}
+                  {formatCurrency(item.price * item.quantity)}
+                </p>
+              </div>
+              <button className="bg-red-600 h8 w-8 rounded-full text-white font-black">
+                X
+              </button>
             </div>
           ))
         )}
